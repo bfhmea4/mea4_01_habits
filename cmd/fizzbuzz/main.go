9944bce40a50f21818fe43d5557b2d9405bfc2ac
Jan Lauber <jan.lauber@protonmail.ch>
2022-09-30 10:05:54 +0200

    add implementation of fizzbuzz with gofiber http server framework
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- cmd/fizzbuzz/main.go
+++ cmd/fizzbuzz/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "github.com/bfhmea4/mea4_01_habits/pkg/fizzbuzz"
+import (
+	"os"
+	"strconv"
+
+	"github.com/bfhmea4/mea4_01_habits/pkg/fizzbuzz"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func main() {
-	fizzbuzz.Calculate()
+	app := fiber.New(fiber.Config{})
+
+	app.Use(cors.New(cors.Config{
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowCredentials: true,
+		AllowOrigins:     "*",
+	}))
+
+	app.Use(logger.New(logger.Config{
+		TimeFormat: "2006/01/02 - 15:04:05",
+		TimeZone:   "Europe/Zurich",
+	}))
+
+	app.Get("/:number", func(c *fiber.Ctx) error {
+		number, err := strconv.Atoi(c.Params("number"))
+
+		if err != nil {
+			return c.Status(400).SendString("Invalid number")
+		}
+
+		return c.SendString(fizzbuzz.Calculate(number))
+	})
+
+	if err := app.Listen(":8000"); err != nil {
+		os.Exit(1)
+	}
 }
--- pkg/fizzbuzz/fizzbuzz.go
+++ pkg/fizzbuzz/fizzbuzz.go
@@ -2,6 +2,18 @@ package fizzbuzz
 
 import "fmt"
 
-func Calculate() {
-	fmt.Println("Hello, world!")
+func Calculate(i int) string {
+	fizz := "fizz"
+	buzz := "buzz"
+
+	if i%3 == 0 && i%5 == 0 {
+		return fizz + buzz
+	}
+	if i%3 == 0 {
+		return fizz
+	}
+	if i%5 == 0 {
+		return buzz
+	}
+	return fmt.Sprintf("%d", i)
 }
