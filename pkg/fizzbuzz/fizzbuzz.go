4cfaec51801611d7c1cacbe7c29100d8b2140558
Jan Lauber <jan.lauber@protonmail.ch>
2022-09-30 10:53:16 +0200

    add testhttpcontroller test
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- pkg/fizzbuzz/fizzbuzz.go
+++ pkg/fizzbuzz/fizzbuzz.go
@@ -1,5 +1,18 @@
 package fizzbuzz
 
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func ServeHTTP() fiber.App {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		// return status 200 with "not implemented" as body
+		return c.SendStatus(fiber.StatusNotImplemented)
+	})
+	return *app
+}
+
 func Calculate(i int) string {
 	return "not implemented"
 }
--- test/fizzbuzz/fizzbuzz_test.go
+++ test/fizzbuzz/fizzbuzz_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"net/http/httptest"
 	"testing"
 
 	"github.com/bfhmea4/mea4_01_habits/pkg/fizzbuzz"
@@ -12,36 +13,10 @@ func TestFizzBuzz(t *testing.T) {
 		i    int
 		want string
 	}{
-		{name: "Test1", i: 1, want: "1"},
-		{name: "Test2", i: 2, want: "2"},
-		{name: "Test3", i: 3, want: "fizz"},
-		{name: "Test4", i: 4, want: "4"},
-		{name: "Test5", i: 5, want: "buzz"},
-		{name: "Test6", i: 6, want: "fizz"},
-		{name: "Test7", i: 7, want: "7"},
-		{name: "Test8", i: 8, want: "8"},
-		{name: "Test9", i: 9, want: "fizz"},
-		{name: "Test10", i: 10, want: "buzz"},
-		{name: "Test11", i: 11, want: "11"},
-		{name: "Test12", i: 12, want: "fizz"},
-		{name: "Test13", i: 13, want: "13"},
-		{name: "Test14", i: 14, want: "14"},
-		{name: "Test15", i: 15, want: "fizzbuzz"},
-		{name: "Test16", i: 16, want: "16"},
-		{name: "Test17", i: 17, want: "17"},
-		{name: "Test18", i: 18, want: "fizz"},
-		{name: "Test19", i: 19, want: "19"},
-		{name: "Test20", i: 20, want: "buzz"},
-		{name: "Test21", i: 21, want: "fizz"},
-		{name: "Test22", i: 22, want: "22"},
-		{name: "Test23", i: 23, want: "23"},
-		{name: "Test24", i: 24, want: "fizz"},
-		{name: "Test25", i: 25, want: "buzz"},
-		{name: "Test26", i: 26, want: "26"},
-		{name: "Test27", i: 27, want: "fizz"},
-		{name: "Test28", i: 28, want: "28"},
-		{name: "Test29", i: 29, want: "29"},
-		{name: "Test30", i: 30, want: "fizzbuzz"},
+		{name: "get String of number if number doesn't match /3 or /5", i: 1, want: "1"},
+		{name: "get buzz if number matches /5", i: 5, want: "buzz"},
+		{name: "get fizz if number matches /3", i: 6, want: "fizz"},
+		{name: "get fizzbuzz if number matches /3 and /5", i: 15, want: "fizzbuzz"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -50,4 +25,57 @@ func TestFizzBuzz(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestHttpController(t *testing.T) {
+	tests := []struct {
+		description  string // description of the test case
+		route        string // route path to test
+		param        string // route parameter to test
+		expectedCode int    // expected HTTP status code
+		expectedBody string // expected HTTP response body
+	}{
+		// First test case
+		{
+			description:  "get HTTP status 200, and body returns the number 1 with parameter 1",
+			route:        "/",
+			param:        "1",
+			expectedCode: 200,
+			expectedBody: "1",
+		},
+		// Second test case
+		{
+			description:  "get HTTP status 404, when route is not exists",
+			route:        "/not-found",
+			expectedCode: 404,
+		},
+		// Third test case
+		{
+			description:  "get HTTP status 400, when param is not a number",
+			route:        "/",
+			param:        "not-a-number",
+			expectedCode: 400,
+		},
+	}
+
+	// Register the http server from main function
+	app := fizzbuzz.ServeHTTP()
+
+	// Run the test cases
+	for _, test := range tests {
+		req := httptest.NewRequest("GET", test.route, nil)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Accept", "application/json")
+		if test.param != "" {
+			req.URL.RawQuery = test.param
+		}
+		res, err := app.Test(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if res.StatusCode != test.expectedCode {
+			t.Errorf("Test case %s failed, expected status code %d, got %d", test.description, test.expectedCode, res.StatusCode)
+		}
+	}
+
 }
