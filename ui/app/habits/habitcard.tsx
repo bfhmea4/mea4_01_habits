9b7b9f34911d727f59100d2e0a44a22a3977aa94
Jan Lauber <jan.lauber@protonmail.ch>
2022-10-30 17:21:26 +0100

    add habit interface and stuff
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

---
+++ ui/app/habits/habitcard.tsx
@@ -0,0 +1,7 @@
+import { Habit } from "../../lib/interfaces";
+
+export interface HabitCardProps {
+  habit: Habit;
+}
+
+export const HabitCard = ({ habit }: HabitCardProps) => {};
---
+++ ui/lib/interfaces.tsx
@@ -0,0 +1,8 @@
+export interface Habit {
+    id: number;
+    title: string;
+    description: string;
+    completed: boolean;
+    created: Date;
+    updated: Date;
+}
