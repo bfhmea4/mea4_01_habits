9737ddeb5344651be40d8a1da16a71d3bc17030d
Jan Lauber <jan.lauber@protonmail.ch>
2022-10-30 20:09:25 +0100

    edit new habit and integrade habit form
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- ui/app/habits/NewHabit.tsx
+++ ui/app/habits/NewHabit.tsx
@@ -1,6 +1,7 @@
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { useRef } from "react";
 import { PopUpModal } from "../general/modals/PopUpModal";
+import { HabitForm } from "./HabitForm";
 
 export const NewHabit = () => {
   const createModalRef = useRef<any>(null);
@@ -14,7 +15,9 @@ export const NewHabit = () => {
       className="habit-card bg-white border-4 border-primary rounded-lg max-w-lg py-2 pr-5 my-4 text-primary shadow-lg select-none cursor-pointer active:hover:scale-105 transition-all duration-200"
       onClick={handleOnClick}
     >
-      <PopUpModal ref={createModalRef}></PopUpModal>
+      <PopUpModal ref={createModalRef}>
+        <HabitForm modalRef={createModalRef} type="create" />
+      </PopUpModal>
       <div className="flex flex-row justify-between items-center px-4">
         <div className="flex flex-row items-center">
           <PlusIcon className="w-6 h-6" />
