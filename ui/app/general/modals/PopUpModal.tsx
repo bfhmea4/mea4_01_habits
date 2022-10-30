0a5f2948868b02c4fca28234e2f3563f49377bd0
Jan Lauber <jan.lauber@protonmail.ch>
2022-10-30 20:07:52 +0100

    add general pop up modal
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- ui/app/general/modals/PopUpModal.tsx
+++ ui/app/general/modals/PopUpModal.tsx
@@ -43,17 +43,17 @@ export const PopUpModal = forwardRef(({ children }: any, ref) => {
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full justify-center p-4 text-center items-center">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              enterFrom="opacity-0 translate-y-0 scale-95"
+              enterTo="opacity-100 translate-y-0 scale-100"
               leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              leaveFrom="opacity-100 translate-y-0scale-100"
+              leaveTo="opacity-0 translate-y-0 scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all my-8 w-full max-w-lg p-6">
                 <XMarkIcon
                   className="absolute top-2 right-2 w-6 h-6 cursor-pointer"
                   onClick={() => setOpen(false)}
