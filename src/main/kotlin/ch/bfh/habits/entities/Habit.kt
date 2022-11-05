1e05d5ec12b4d79a90562c40a87db3da0899fe44
Andy Bigler <andy.bigler@swisscom.com>
2022-11-05 14:58:06 +0100

    Support on delete set null

--- src/main/kotlin/ch/bfh/habits/entities/Habit.kt
+++ src/main/kotlin/ch/bfh/habits/entities/Habit.kt
@@ -1,5 +1,6 @@
 package ch.bfh.habits.entities
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import java.sql.Timestamp
@@ -16,5 +17,13 @@ class Habit (
     @CreationTimestamp
     var createdAt: Timestamp? = null,
     @UpdateTimestamp
-    var editedAt: Timestamp? = null
-)
+    var editedAt: Timestamp? = null,
+    @OneToMany(mappedBy = "habit", cascade = [CascadeType.PERSIST])
+    @JsonIgnore
+    var journalEntries: MutableList<JournalEntry> = mutableListOf()
+) {
+    @PreRemove
+    private fun preRemove() {
+        journalEntries.forEach { child -> child.habit = null }
+    }
+}
