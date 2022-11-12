fb8040f02c9842269423f28825c3c27cac40bd34
Andy Bigler <andy.bigler@swisscom.com>
2022-11-12 11:44:25 +0100

    styling journal cards

--- ui/components/journalEntry/JournalEntryCard.tsx
+++ ui/components/journalEntry/JournalEntryCard.tsx
@@ -7,27 +7,30 @@ export interface JournalEntryCardProps {
 }
 
 export const JournalEntryCard = ({ journalEntry }: JournalEntryCardProps) => {
-  console.log(journalEntry)
   return (
     <div>
       <div className="flex flex-row flex-wrap mt-2">
-        <div className="basis-1/6 text-center">
+        <div className="basis-1/6 text-center h-[35px]">
           {journalEntry.description ? (
-            <Image src="/images/icons/book.svg" alt="Personal Goals" width={40} height={35} unoptimized />
+            <Image src="/images/icons/book.svg" alt="Book" width={40} height={35} unoptimized />
           ) : (
-            <Image src="/images/icons/checked.svg" alt="Personal Goals" width={40} height={35} unoptimized />
+            <Image src="/images/icons/checked.svg" alt="Checked" width={40} height={35} unoptimized />
           )}
         </div>
-        <div className="basis-5/6">
-          <h2 className="font-normal text-lg">
-            {journalEntry.id} - {journalEntry?.habit?.title ?? 'No habit'}
+        <div className="basis-5/6 self-center">
+          <h2 className="text-sm">
+            {journalEntry.createdAt.toLocaleDateString('en-US', {
+              month: 'short',
+              day: 'numeric',
+            })}
+            - {journalEntry?.habit?.title ?? 'No habit'}
           </h2>
         </div>
-        <div className="basis-1/6 flex flex-row min-h-[1rem]">
+        <div className="basis-1/6 flex flex-row min-h-[1.5rem] pt-2">
           <div className="basis-1/2 border-r-2 border-grey"></div>
           <div className="basis-1/2"></div>
         </div>
-        <div className="basis-5/6">
+        <div className="basis-5/6 py-2">
           <p>{journalEntry.description}</p>
         </div>
       </div>
