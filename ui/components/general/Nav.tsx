166bffdeffd581adc8f093421bd7bf34ec88b9c1
Jan Lauber <jan.lauber@protonmail.ch>
2022-11-27 14:55:06 +0100

    feat: integrate select component
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- ui/components/general/Nav.tsx
+++ ui/components/general/Nav.tsx
@@ -1,14 +1,28 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useRef } from 'react'
 import { classNames } from '../../lib/design'
+import { JournalEntryForm } from '../journalEntry/JournalEntryForm'
+import { PopUpModal } from './modals/PopUpModal'
 
 export const Nav = () => {
+  const createModalRef = useRef<any>(null)
 
   const router = useRouter()
 
+  const handleCreate = () => {
+    createModalRef.current.open()
+  }
+
   return (
     <div className='relative'>
+
+      <PopUpModal ref={createModalRef}>
+        <JournalEntryForm modalRef={createModalRef} type="create" />
+      </PopUpModal>
+
+
       <Link href="/" className={classNames(
         router.pathname == '/' ? 'hidden' : 'hidden',
       )}>
@@ -17,11 +31,22 @@ export const Nav = () => {
         </button>
       </Link>
 
-      <Link href="/journal">
-        <button className="z-10 fixed bottom-6 right-6 rounded-full bg-white border-4 border-primary p-3 cursor-pointer active:hover:scale-105 transition-all duration-150 ease-in-out">
-          <Image src="/images/icons/notebook.svg" alt="Personal Goals" width={40} height={35} unoptimized />
-        </button>
-      </Link>
+      {
+        router.pathname == '/journal' ? (
+          <button
+            className="z-10 fixed bottom-6 right-6 rounded-full bg-white border-4 border-primary p-3 cursor-pointer active:hover:scale-105 transition-all duration-150 ease-in-out"
+            onClick={() => handleCreate()}
+          >
+            <Image src="/images/icons/notebook.svg" alt="Personal Goals" width={40} height={35} unoptimized />
+          </button>
+        ) : (
+          <Link href="/journal">
+            <button className="z-10 fixed bottom-6 right-6 rounded-full bg-white border-4 border-primary p-3 cursor-pointer active:hover:scale-105 transition-all duration-150 ease-in-out">
+              <Image src="/images/icons/notebook.svg" alt="Personal Goals" width={40} height={35} unoptimized />
+            </button>
+          </Link>
+        )
+      }
     </div>
   )
 }
