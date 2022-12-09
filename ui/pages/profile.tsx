adb238adb77d78be4f5761b765b9d29f3dc3d84b
Jan Lauber <jan.lauber@protonmail.ch>
2022-12-09 13:56:58 +0100

    feat: add name of user
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- ui/pages/profile.tsx
+++ ui/pages/profile.tsx
@@ -1,4 +1,4 @@
-import { EnvelopeIcon, FingerPrintIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon, FingerPrintIcon, UserIcon } from "@heroicons/react/24/outline";
 import { Tooltip } from "@mui/material";
 import StyledButton, { StyledButtonType } from "../components/general/buttons/StyledButton";
 import { useUserContext } from "../context/userContext";
@@ -16,24 +16,21 @@ const Profile = () => {
                 <div className="max-w-xl">
                     <div className="top-0 absolute sm:max-w-xl w-full">
                         <div className="pl-6 pt-6">
-                            <h2 className="text-sm text-gray-600">
-                                Profile
-                            </h2>
                             <h1 className="text-4xl font-medium">
-                                Hi, <span className="text-primary">{!loading && user && user.firstName}</span>
+                                Profile
                             </h1>
                         </div>
                     </div>
                 </div>
                 <div
-                    className="mt-48 flex flex-col space-y-2 justify-between items-center px-6"
+                    className="mt-20 flex flex-col space-y-2 justify-between items-center px-6"
                 >
-                    <h2 className="text-2xl font-medium w-full">Settings</h2>
                     <div
                         className="w-full flex flex-col space-y-2"
                     >
-                    <p><Tooltip title="User ID" placement="top"><FingerPrintIcon className="w-5 h-5 inline-block" /></Tooltip> {user?.id}</p>
-                    <p><Tooltip title="Email" placement="top"><EnvelopeIcon className="w-5 h-5 inline-block" /></Tooltip> {user?.email}</p>
+                        <p><Tooltip title="Name" placement="top"><UserIcon className="w-5 h-5 inline-block" /></Tooltip> {user?.firstName} {user?.lastName}</p>
+                        <p><Tooltip title="User ID" placement="top"><FingerPrintIcon className="w-5 h-5 inline-block" /></Tooltip> {user?.id}</p>
+                        <p><Tooltip title="Email" placement="top"><EnvelopeIcon className="w-5 h-5 inline-block" /></Tooltip> {user?.email}</p>
 
                     </div>
                     <StyledButton
