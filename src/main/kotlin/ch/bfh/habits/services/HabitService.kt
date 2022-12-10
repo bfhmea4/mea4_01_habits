7846ba246688ee6321d22688fccd35a96e5c91f7
Jan Fuhrer <janfuhrer@mailbox.org>
2022-12-10 11:13:30 +0100

    fix: false rebasing

--- src/main/kotlin/ch/bfh/habits/services/HabitService.kt
+++ src/main/kotlin/ch/bfh/habits/services/HabitService.kt
@@ -50,7 +50,7 @@ class HabitService(private val habitDAO: HabitDAO) {
     }
 
     @Transactional
-    fun updateHabitById(id: Long, habitDTO: HabitDTO) {
+    fun updateHabit(id: Long, habitDTO: HabitDTO, userId: Long) {
         if ((habitDTO.frequencyValue != null && habitDTO.frequency == null) || (habitDTO.frequencyValue == null && habitDTO.frequency != null)) {
             throw BadRequestException("Frequency and frequencyValue must be set together")
         }
