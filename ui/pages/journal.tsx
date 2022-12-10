6bb53e76484f45fa5ecb246f806ac7911916f44b
Andy Bigler <andy.bigler@swisscom.com>
2022-12-10 12:29:49 +0100

    Change frontend after backend refactoring

--- ui/pages/journal.tsx
+++ ui/pages/journal.tsx
@@ -67,27 +67,24 @@ const Home: NextPage = () => {
     ;(async () => {
       try {
         const { data } = await Api.get('/journal_entries')
-        if (data.journalEntries) {
-          // validate data.journalEntries if its object of JournalEntry[]
-          if (data.journalEntries) {
-            try {
-              const journalEntries = data.journalEntries.map((journalEntry: JournalEntry) => {
-                return {
-                  ...journalEntry,
-                  createdAt: new Date(journalEntry.createdAt),
-                  editedAt: new Date(journalEntry.editedAt),
-                }
-              })
-              // sort habits by createdAt latest first
-              journalEntries.sort((a: any, b: any) => {
-                return b.createdAt.getTime() - a.createdAt.getTime()
-              })
+        if (data) {
+          try {
+            const journalEntries = data.map((journalEntry: JournalEntry) => {
+              return {
+                ...journalEntry,
+                createdAt: new Date(journalEntry.createdAt),
+                editedAt: new Date(journalEntry.editedAt),
+              }
+            })
+            // sort habits by createdAt latest first
+            journalEntries.sort((a: any, b: any) => {
+              return b.createdAt.getTime() - a.createdAt.getTime()
+            })
 
-              setJournalEntries(journalEntries)
-              setFilteredJournalEntries(journalEntries)
-            } catch (error) {
-              console.log(error)
-            }
+            setJournalEntries(journalEntries)
+            setFilteredJournalEntries(journalEntries)
+          } catch (error) {
+            console.log(error)
           }
         }
       } catch (error) {
@@ -99,26 +96,23 @@ const Home: NextPage = () => {
     ;(async () => {
       try {
         const { data } = await Api.get('/habits')
-        if (data.habits) {
-          // validate data.habits if its object of Habit[]
-          if (data.habits) {
-            try {
-              const habits = data.habits.map((habit: Habit) => {
-                return {
-                  ...habit,
-                  createdAt: new Date(habit.createdAt),
-                  editedAt: new Date(habit.editedAt),
-                }
-              })
-              // sort habits by createdAt latest first
-              habits.sort((a: any, b: any) => {
-                return b.createdAt.getTime() - a.createdAt.getTime()
-              })
+        if (data) {
+          try {
+            const habits = data.map((habit: Habit) => {
+              return {
+                ...habit,
+                createdAt: new Date(habit.createdAt),
+                editedAt: new Date(habit.editedAt),
+              }
+            })
+            // sort habits by createdAt latest first
+            habits.sort((a: any, b: any) => {
+              return b.createdAt.getTime() - a.createdAt.getTime()
+            })
 
-              setHabits(habits)
-            } catch (error) {
-              console.log(error)
-            }
+            setHabits(habits)
+          } catch (error) {
+            console.log(error)
           }
         }
       } catch (error) {
