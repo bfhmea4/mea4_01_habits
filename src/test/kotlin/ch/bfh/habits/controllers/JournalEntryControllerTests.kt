1b1e33750cdf4bb3d43cd2db29688903d8bb1b97
Andy Bigler <andy.bigler@swisscom.com>
2022-12-11 10:59:26 +0100

    Add more tests

--- src/test/kotlin/ch/bfh/habits/controllers/JournalEntryControllerTests.kt
+++ src/test/kotlin/ch/bfh/habits/controllers/JournalEntryControllerTests.kt
@@ -2,6 +2,7 @@ package ch.bfh.habits.controllers
 
 import ch.bfh.habits.entities.Habit
 import ch.bfh.habits.entities.JournalEntry
+import ch.bfh.habits.exceptions.EntityNotFoundException
 import ch.bfh.habits.services.JournalEntryService
 import ch.bfh.habits.services.UserService
 import ch.bfh.habits.util.TokenProvider
@@ -65,5 +66,62 @@ internal class JournalEntryControllerTests {
         // then
         assertEquals(mapper.writeValueAsString(arrayListOf(journalEntry)), result.response.contentAsString)
         verify { service.getAllJournalEntries(1) }
+    }
+
+    @Test
+    @WithMockUser(username = "correctUser")
+    fun `getJournalEntry returns the requested journal entry`() {
+        // given
+        val habit = Habit("Gym", "Go to the gym",1, userId = 1)
+        val journalEntry = JournalEntry("Done", habit, 1)
+        every { service.getJournalEntry(1, 1) } returns journalEntry
+        val userName = SecurityContextHolder.getContext().authentication.name
+
+        // when
+        val result = mockMvc.perform(get("/api/journal_entry/1")
+            .header("Authorization", userName))
+            .andExpect(status().isOk).andReturn()
+
+        // then
+        assertEquals(mapper.writeValueAsString(journalEntry), result.response.contentAsString)
+        verify { service.getJournalEntry(1, 1) }
+    }
+
+    @Test
+    @WithMockUser(username = "wrongUser")
+    fun `getJournalEntry returns not found if user is not allowed to access journal entry`() {
+        // given
+        val habit = Habit("Gym", "Go to the gym",1, userId = 1)
+        val journalEntry = JournalEntry("Done", habit, 1)
+        every { service.getJournalEntry(1, 1) } returns journalEntry
+        every { service.getJournalEntry(1, 2) } throws  EntityNotFoundException("")
+        val userName = SecurityContextHolder.getContext().authentication.name
+
+        // when
+        mockMvc.perform(get("/api/journal_entry/1")
+            .header("Authorization", userName))
+            .andExpect(status().isNotFound).andReturn()
+
+        // then
+        verify { service.getJournalEntry(1, 2) }
+    }
+
+    @Test
+    @WithMockUser(username = "correctUser")
+    fun `getJournalEntry returns not found if habit does not exist`() {
+        // given
+        val habit = Habit("Gym", "Go to the gym",1, userId = 1)
+        val journalEntry = JournalEntry("Done", habit, 1)
+        every { service.getJournalEntry(1, 1) } returns journalEntry
+        every { service.getJournalEntry(2, 1) } throws  EntityNotFoundException("")
+        val userName = SecurityContextHolder.getContext().authentication.name
+
+        // when
+        mockMvc.perform(get("/api/journal_entry/2")
+            .header("Authorization", userName))
+            .andExpect(status().isNotFound).andReturn()
+
+        // then
+        verify { service.getJournalEntry(2, 1) }
     }
 }
